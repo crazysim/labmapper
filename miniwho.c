9cb4b7f54b6969ee2e740556ad9077a187f8aad2
Nelson Chen <crazysim@gmail.com>
2012-04-21 02:15:59 -0700

    test for flags

--- miniwho.c
+++ miniwho.c
@@ -6,8 +6,10 @@ struct utmpx *getutent();
 
 int main(){
     utmpp = getutxent();
-    while ((utmpp = getutxent())!= (struct utmp *) NULL) {
-        printf("%s\n", utmpp->ut_user);
+    while ((utmpp = getutxent())!= (struct utmpx *) NULL) {
+        if (utmpp->ut_type == USER_PROCESS){
+            printf("%s %s\n", utmpp->ut_user, utmpp->ut_host);
+        }
     }
     return 0;
 }
